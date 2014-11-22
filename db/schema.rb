64760e72a55717e1ad904bcd9cc7ef603772f950
Matthias Flaig <matt.flaig@web.de>
2014-11-22 22:25:24 +0100

    new form for bug listing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120194953) do
+ActiveRecord::Schema.define(version: 20141122194341) do
 
   create_table "bugs", force: true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20141120194953) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "filename"
   end
 
   create_table "solutions", force: true do |t|
